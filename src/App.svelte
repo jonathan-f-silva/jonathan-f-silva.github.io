c2172c7ab3cf703b70baf46b2903a9fcc96a069c
Jonathan Ferreira <j0n4t@hotmail.com>
2022-06-05 17:14:05 -0300

    ✨ feat: adiciona Svelte

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import Hero from "./lib/Hero.svelte";
+</script>
+
+<main>
+  <Hero />
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+</style>
