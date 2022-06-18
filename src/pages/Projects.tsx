ee27434086b461ca9ea8dacc1993cf872dd0a588
Jonathan Ferreira <j0n4t@hotmail.com>
2022-06-18 15:13:45 -0300

    🚚 move: project list to JSON

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -2,21 +2,7 @@ import { Heading, VStack } from '@chakra-ui/react';
 
 import Project from '../components/Project';
 
-const projects = [
-  {
-    name: 'Ebytr ToDo',
-    link: 'https://jonathan-f-silva.github.io/project-ebytr-ui',
-    // eslint-disable-next-line max-len
-    logo: 'https://raw.githubusercontent.com/jonathan-f-silva/project-ebytr-ui/7213d1092ad8fc5ffb4cc8cc553ae0d462dcff2b/src/assets/ebytr.svg',
-    descriptions: [
-      `Aplicação estilo lista de tarefas feita para o
-      Desafio da Blitz de Carreira da Trybe.`,
-      `Com esse app é possível:
-       Listar e ordenar tarefas pela descrição, data de criação e status,
-       criar, editar e remover tarefas e alterar status de uma tarefa`,
-    ],
-  },
-];
+import projectList from '../utils/projectList.json';
 
 function Projects() {
   return (
@@ -27,7 +13,7 @@ function Projects() {
       >
         Projetos
       </Heading>
-      {projects.map(
+      {projectList.map(
         (project) => <Project key={ project.name } { ...project } />,
       )}
     </VStack>
