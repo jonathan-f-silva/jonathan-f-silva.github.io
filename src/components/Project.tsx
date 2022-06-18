714051263a9ff6d0603ef12e9ca92409c8827f1f
Jonathan Ferreira <j0n4t@hotmail.com>
2022-06-18 17:52:34 -0300

    ✨ feat: adiciona GitHub link dos projetos

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,20 +1,22 @@
 import {
   Stack, HStack, VStack, Center, Flex,
   useRadioGroup, useColorModeValue,
-  Heading, Text, Image,
+  Heading, Text, Image, Link, Icon,
 } from '@chakra-ui/react';
 import { useState } from 'react';
+import { BsGithub } from 'react-icons/bs';
 import { MdDesktopMac, MdOutlineSmartphone, MdTabletMac } from 'react-icons/md';
 import RadioCard from './RadioCard';
 
 type ProjectProps = {
   name: string,
   link: string,
+  ghLink: string,
   logo: string,
   descriptions: string[],
 }
 
-function Project({ name, link, logo, descriptions }: ProjectProps) {
+function Project({ name, link, ghLink, logo, descriptions }: ProjectProps) {
   const options = [
     {
       label: 'smart phone',
@@ -40,55 +42,59 @@ function Project({ name, link, logo, descriptions }: ProjectProps) {
   });
 
   return (
-    <VStack w="full" p={ { base: '2', md: '5' } }>
-      <Stack
-        direction={ { base: 'column', md: 'row' } }
-        w="full"
-        h="full"
-        alignItems="center"
-        justifyContent="space-evenly"
-        spacing="5"
-      >
-        <VStack maxW="350px">
-          <Image
-            alt={ `Logo do projeto ${name}` }
-            // eslint-disable-next-line max-len
-            src={ logo }
+    <Stack
+      p={ { base: '2', md: '5' } }
+      direction={ { base: 'column', md: 'row' } }
+      w="full"
+      h="full"
+      alignItems="center"
+      justifyContent="space-evenly"
+      spacing="5"
+    >
+      <VStack maxW="350px">
+        <Image
+          alt={ `Logo do projeto ${name}` }
+          // eslint-disable-next-line max-len
+          src={ logo }
+        />
+        <Link href={ ghLink }>
+          <Heading>
+            <Icon as={ BsGithub } h="0.8em" mx="1" />
+            {name}
+          </Heading>
+        </Link>
+        <HStack display={ { base: 'none', lg: 'flex' } }>
+          {options.map((value) => {
+            const label = value.width;
+            const radio = getRadioProps({ value: label });
+            return (
+              <RadioCard key={ value.label } { ...radio }>
+                {value.icon}
+              </RadioCard>
+            );
+          })}
+        </HStack>
+        {descriptions.map((description) => (
+          <Text key={ description }>{description}</Text>
+        )) }
+      </VStack>
+      <Center h="500" w={ { base: 'full', lg: '65%' } }>
+        <Flex
+          w={ viewWidth }
+          h="full"
+          bg={ useColorModeValue('gray.100', 'gray.900') }
+          borderRadius="2xl"
+          p="2"
+        >
+          <iframe
+            width="100%"
+            height="100%"
+            src={ link }
+            title={ name }
           />
-          <Heading>{ name }</Heading>
-          <HStack display={ { base: 'none', lg: 'flex' } }>
-            {options.map((value) => {
-              const label = value.width;
-              const radio = getRadioProps({ value: label });
-              return (
-                <RadioCard key={ value.label } { ...radio }>
-                  {value.icon}
-                </RadioCard>
-              );
-            })}
-          </HStack>
-          {descriptions.map((description) => (
-            <Text key={ description }>{description}</Text>
-          )) }
-        </VStack>
-        <Center h="500" w={ { base: 'full', lg: '65%' } }>
-          <Flex
-            w={ viewWidth }
-            h="full"
-            bg={ useColorModeValue('gray.100', 'gray.900') }
-            borderRadius="2xl"
-            p="2"
-          >
-            <iframe
-              width="100%"
-              height="100%"
-              src={ link }
-              title={ name }
-            />
-          </Flex>
-        </Center>
-      </Stack>
-    </VStack>
+        </Flex>
+      </Center>
+    </Stack>
   );
 }
 
